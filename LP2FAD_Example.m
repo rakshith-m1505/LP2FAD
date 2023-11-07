547ad2496e7bb61a2ee088fd566f76d6d72faa11
Rakshith Manikandan <150019041+rakshith-m1505@users.noreply.github.com>
2023-11-07 22:25:20 +0100

    Example MATLAB code on how to use the function LP2FAD

--- LP2FAD_Example.m
+++ LP2FAD_Example.m
@@ -5,16 +5,16 @@ clear all;
 %% Generate Target LPs and Define N (INPUT)
 
 % Target In-Plane LPs [V1,V2,V3,V4]
-LP_targets = [0.523205; 0; 0.150000; 0]; % Example case for Symmetric-Balanced FAD
+LP_targets = [0.523205; 0; 0.150000; 0];  % Uncomment line for Symmetric-Balanced FAD Example case
 
-% LP_targets= [0.4964; 0.2098; -0.2500; 0.3464]; % Example case for Symmetric-Unbalanced FAD
+% LP_targets= [0.4964; 0.2098; -0.2500; 0.3464]; % Uncomment line for Symmetric-Unbalanced FAD Example case
 
 % Desired angle multiples in Layup <45 / 30 / 15>
 angle_multiples = 15;
 
-% Design Guidelines
+% Design Guidelines <true/false>
 symmetry = true;
-balanced = true;
+balanced = true; % Enter false for Symmetric-Unbalanced FAD design
 
 % Number of Layers to be designed
 D_N = 10;
